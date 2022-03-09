c43028a057452b79620e689cea2adc4d10ec3b0b
EHoldener <eholdener12@gmail.com>
2022-03-08 22:09:14 -0800

    created tracking of inputs, through storing as one long string. dom display automatically updates with each button press next is to iterate through string of numbers and symbols and determine how many symbols there are, what order left to right, etc

--- scripts.js
+++ scripts.js
@@ -14,35 +14,59 @@ function divide(a, b) {
     return (a/b);
 }
 
-function operate(operator, a, b) {
-    switch (operator) {
-        case '+':
-            add(a, b);
-        case '-':
-            subtract(a, b);
-        case '*':
-            multiply(a, b);
-        case '/':
-            divide(a, b);
-    }
+function operate(keys) {
+    const keysTranslation = keys.split("");
+    console.log(keysTranslation);
+
+    // keysTranslation.forEach(key => )
+    // switch (operator) {
+    //     case '+':
+    //         add(a, b);
+    //     case '-':
+    //         subtract(a, b);
+    //     case '*':
+    //         multiply(a, b);
+    //     case '/':
+    //         divide(a, b);
+    // }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const btns = document.querySelectorAll('.calc-button');
-
     btns.forEach(btn => {
         btn.addEventListener('click', function (e) {
-            console.log(e.target.getAttribute('data-key'));
-
+            collector(e.target.getAttribute('data-key'));
         });
     });
 });
 
+let keys = "";
 function collector(key) {
-    let keys = [];
+    const display = document.querySelector(".display");
+    let displayNumber = document.createElement("p");
+    displayNumber.className = ".display-number";
+    if (display.hasChildNodes) {
+        while (display.firstChild) {
+            display.removeChild(display.firstChild);
+        }
+    }
+
+
     if (key === "=") {
-        operate();
+        passKeys = keys;
+        keys = "";
+        operate(passKeys);
+    } else if (key === "clear") {
+        keys = "";
+        displayNumber.textContent = keys;
+        display.appendChild(displayNumber);
+    } else if (key === "delete") {
+        keys = keys.substring(0, keys.length - 1);
+        displayNumber.textContent = keys;
+        display.appendChild(displayNumber);
     } else {
-        
+        keys = keys + key;
+        displayNumber.textContent = keys;
+        display.appendChild(displayNumber);
     }
 }
