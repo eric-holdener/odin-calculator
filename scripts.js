fa0aaff26888b01262a231bc40c30a3a64a71027
EHoldener <eholdener12@gmail.com>
2022-03-10 19:20:35 -0800

    added single operator functionality, commented code for readability

--- scripts.js
+++ scripts.js
@@ -15,13 +15,17 @@ function divide(a, b) {
 }
 
 function stringParsing(keys) {
+    // splits string of keys into an array of individual letters
     const keysTranslation = keys.split("");
 
+    // initialize keyIndex as object and keysStore as array to store the objects in
     let keyIndex = {};
     let keysStore = [];
 
+    // intermediate calculator is for multi operator functions
     let intermediateCalculator = 0;
 
+    // iterates through array passed in (keys) and returns all operators and their indexes
     keysTranslation.forEach((key, index) => {
         if (key === "+" || key === "-" || key === "/" || key === "*") {
             keyIndex = {key, index};
@@ -29,33 +33,48 @@ function stringParsing(keys) {
         }
     });
 
-    for (let i = 0; i < keysStore.length; i++){
-        if (i == 0) {
-            a = keysTranslation.slice(0, keysStore[i]["index"]).join('');
-            a = parseFloat(a);
-
-            b = keysTranslation.slice(keysStore[i]["index"]+1, keysStore[i+1]["index"]).join('');
-            b = parseFloat(b);
-
-            intermediateCalculator = operate(keysStore[i]["key"], a, b);
-        } else if (i == keysStore.length-1) {
-            a = intermediateCalculator;
-
-            b = keysTranslation.slice(keysStore[i]["index"]+1).join('');
-            b = parseFloat(b);
-
-            intermediateCalculator = (operate(keysStore[i]["key"], a, b));
-        } else {
-            a = intermediateCalculator;
-
-            b = keysTranslation.slice(keysStore[i]["index"]+1, keysStore[i+1]["index"]).join('');
-            b = parseFloat(b);
-
-            intermediateCalculator = operate(keysStore[i]["key"], a, b);
+    // logic for multi operator functions passed in
+    // unique logic for determining a and b for first operator of string, last operator of string, and middle operators
+    if (keysStore.length > 1) {
+        for (let i = 0; i < keysStore.length; i++){
+            if (i == 0) {
+                a = keysTranslation.slice(0, keysStore[i]["index"]).join('');
+                a = parseFloat(a);
+    
+                b = keysTranslation.slice(keysStore[i]["index"]+1, keysStore[i+1]["index"]).join('');
+                b = parseFloat(b);
+    
+                intermediateCalculator = operate(keysStore[i]["key"], a, b);
+            } else if (i == keysStore.length-1) {
+                a = intermediateCalculator;
+    
+                b = keysTranslation.slice(keysStore[i]["index"]+1).join('');
+                b = parseFloat(b);
+    
+                intermediateCalculator = (operate(keysStore[i]["key"], a, b));
+            } else {
+                a = intermediateCalculator;
+    
+                b = keysTranslation.slice(keysStore[i]["index"]+1, keysStore[i+1]["index"]).join('');
+                b = parseFloat(b);
+    
+                intermediateCalculator = operate(keysStore[i]["key"], a, b);
+            }
         }
+    } 
+    // logic for single operator functions
+    else {
+        a = keysTranslation.slice(0, keysStore[i]["index"]).join('');
+        a = parseFloat(a);
+    
+        b = keysTranslation.slice(keysStore[i]["index"]+1, keysStore[i+1]["index"]).join('');
+        b = parseFloat(b);
+    
+        intermediateCalculator = operate(keysStore[i]["key"], a, b);
     }
 }
 
+// switch statement that takes an operator passed in and performs the correct operation based on operator
 function operate(operator, a, b) {
     console.log("in operate");
     let value = 0;
@@ -79,6 +98,7 @@ function operate(operator, a, b) {
     }
 }
 
+// wait for dom content to load, then select all buttons and add click listeners to them to collect their data-key
 document.addEventListener("DOMContentLoaded", () => {
     const btns = document.querySelectorAll('.calc-button');
     btns.forEach(btn => {
@@ -88,6 +108,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// gets display, deletes the display if there is a child node, then performs different functions on the display of the calculator
+// based on what button was pressed - clear, delete, =, or any other button
 let keys = "";
 function collector(key) {
     const display = document.querySelector(".display");
@@ -98,7 +120,6 @@ function collector(key) {
             display.removeChild(display.firstChild);
         }
     }
-
 
     if (key === "=") {
         passKeys = keys;
